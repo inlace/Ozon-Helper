b6acdde7d7ff4471f1c52a588ea246f6bfd3dc5c
inlace <31384129+inlace@users.noreply.github.com>
2020-10-26 01:29:33 +0300

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -10,18 +10,23 @@
 // @run-at		 document-end
 // @grant        unsafeWindow
 // @grant        window.close
+// @date		2020-10-26
+// @updateURL	https://github.com/inlace/Ozon-Helper/raw/main/userscript.js
 // ==/UserScript==
 
-const html = document.getElementsByTagName('head')[0].innerHTML
-const body = document.getElementsByTagName('body')[0].innerHTML
-//const id = html.match(/"sku":"(.*)"}</)[1]
-const max = [...body.matchAll(/maxQuantity&quot;:(\d+),/g)]
+(function() {
+    'use strict';
+    const html = document.getElementsByTagName('head')[0].innerHTML
+    const body = document.getElementsByTagName('body')[0].innerHTML
+    //const id = html.match(/"sku":"(.*)"}</)[1]
+    const max = [...body.matchAll(/maxQuantity&quot;:(\d+),/g)]
 
-function calculate() {
-    for (var i = 0; i < document.querySelectorAll('.a8h4').length; i++) {
-        if (max[i][1] > 1) {
-            document.querySelectorAll('.a8h4')[i].innerHTML += "Осталось " + max[i][1] + "  шт."
+    function calculate() {
+        for (var i = 0; i < document.querySelectorAll('.a8h4').length; i++) {
+            if (max[i][1] > 1) {
+                document.querySelectorAll('.a8h4')[i].innerHTML += "Осталось " + max[i][1] + "  шт."
+            }
         }
     }
-}
-setTimeout(calculate, 2000);
+    setTimeout(calculate, 2000);
+})();
